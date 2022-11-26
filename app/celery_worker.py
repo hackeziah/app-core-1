35aa7b292ddf2f541aee4de2cac4b6deddf4c1f6
Kevin Paul Lamadrid <lamadridkevinpaul@gmail.com>
2022-11-27 01:06:00 +0800

    refactoring docker and celery task

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -9,10 +9,18 @@ load_dotenv(".env")
 celery = Celery(__name__)
 
 celery.conf.broker_read_url = os.environ.get("CELERY_BROKER_URL")
-celery.conf.result_backend = os.environ.get("CELERY_BROKER_BACKEND")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 
 @celery.task(name="create_task")
 def create_task(a, b, c):
     time.sleep(a)
     return b + c
+
+
+@celery.task(name="generate_analysis")
+def create_task(a, b, c):
+    time.sleep(a)
+    return b + c
+
+
